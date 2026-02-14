bdce658343ca98c94ecd3717840d063c5bd82f34
ohnonononon <nivohq@gmail.com>
2026-02-14 15:06:32 +0100

    dev: talking the idea

--- src/interaction_utils.c
+++ src/interaction_utils.c
@@ -6,7 +6,7 @@
 /*   By: ohnonon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 19:08:12 by ohnonon           #+#    #+#             */
-/*   Updated: 2026/02/14 14:02:19 by ohnonon          ###   ########.fr       */
+/*   Updated: 2026/02/14 14:59:48 by ohnonon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 static int	is_diag_touch(t_const c, t_map *map, t_fpair p, t_fpair mv)
 {
 	t_fpair	next;
-	t_ipair	point;
+	t_fpair	point;
 
 	next.x = p.x + mv.x + 0.1;
 	next.y = p.y + mv.y + 0.1;
-	point.x = next.x + c.pl_radius / 2.0;
-	point.y = next.y - c.pl_radius / 2.0;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.x = next.x + (c.pl_radius / 2.0);
+	point.y = next.y + (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
-	point.x = next.x - c.pl_radius / 2.0;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.x = next.x - (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
-	point.y = next.y + c.pl_radius / 2.0;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.y = next.y - (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
-	point.x = next.x + c.pl_radius / 2.0;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.x = next.x + (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
 	return (0);
 }
@@ -38,32 +38,31 @@ static int	is_diag_touch(t_const c, t_map *map, t_fpair p, t_fpair mv)
 static int	is_cardinal_touch(t_const c, t_map *map, t_fpair p, t_fpair mv)
 {
 	t_fpair	next;
-	t_ipair	point;
+	t_fpair	point;
 
 	next.x = p.x + mv.x + 0.1;
 	next.y = p.y + mv.y + 0.1;
-	point.x = next.x ;
-	point.y = next.y;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.x = next.x + (c.pl_radius / 2.0);
+	point.y = next.y + (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
-	point.x = next.x - c.pl_radius;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.y = next.y - (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
-	point.x = next.x;
-	point.y = next.y + c.pl_radius;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.x = next.x - (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
-	point.y = next.y - c.pl_radius;
-	if (map->grid[point.y / c.tile_size][point.x / c.tile_size] == '1')
+	point.y = next.y + (c.pl_radius / 2.0);
+	if (map->grid[(int)point.y / c.tile_size][(int)point.x / c.tile_size] == '1')
 		return (1);
 	return (0);
 }
 
 int	retrieve_tile(t_const c, t_map *map, t_fpair p, t_fpair mv)
 {
-	if (is_cardinal_touch(c, map, p, mv))
-		return (0);
 	if (is_diag_touch(c, map, p, mv))
+		return (0);
+	else if (is_cardinal_touch(c, map, p, mv))
 		return (0);
 	return (1);
 }
